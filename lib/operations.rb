51a3366ee474a28b1e1cbbf33588ad79b5769ca5
balanced-anchor-3429 <sahnunhm@gmail.com>
2019-04-22 22:40:55 +0000

    Automatically backed up by Learn

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,11 +1,17 @@
-def unsafe?(speed)
+require_relative = '../lib/operations.rb'
 
+def unsafe?(speed)
+if 40 > speed > 60 
+  return false
+else
+  return true
+end
 end
 
 
-
 def not_safe?(speed)
-	
+	40 > speed > 60? return false : return true
+	end
 end
 	
 
